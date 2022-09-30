c7c8af6eec3243c58119dae07a9f32c391527688
Your Name <you@example.com>
2022-09-30 17:52:04 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+        <h1 className="justify-center items-center flex h-screen" >hello boss how are you</h1>
+    </Fragment>
+  );
+}
+
+export default App;
